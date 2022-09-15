2962f0270b5b96869184c1c6ea05288b645eaa70
msenderovsky <msenderovsky@gmail.com>
2022-09-15 15:40:18 -0300

    Delete desafio16 directory

--- desafio16/src/models/carts.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require('mongoose')
-
-
-const cartSchema = Schema({
-    date: Date,
-    products: [{ type: Schema.Types.ObjectId, ref: 'products' }]
-});
-
-module.exports = model('carts', cartSchema);
