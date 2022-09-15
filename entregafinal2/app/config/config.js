a3206839122e3d8f809fe0872ea79161daf8b2aa
msenderovsky <msenderovsky@gmail.com>
2022-09-15 15:42:02 -0300

    Delete entregafinal2 directory

--- entregafinal2/app/config/config.js
+++
@@ -1,4 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./e-commerce-27e4b-firebase-adminsdk-b1ty9-2cc79cf047.json");
-
-module.exports = {admin, serviceAccount}
--- entregafinal2/index.js
+++
@@ -1,8 +0,0 @@
-//const mongoose= require ('mongoose')
-const app= require ('./app/app')
-const PORT=process.env.PORT
-//const urlBase=process.env.DB
-
-//mongoose.connect(urlBase)
-
-app.listen(PORT, ()=> console.log(`http://localhost:${PORT}`))
