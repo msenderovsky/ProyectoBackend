31aaed901c96bfb36a61b01d1c5e3ca73fced2a2
msenderovsky <msenderovsky@gmail.com>
2022-09-15 15:41:42 -0300

    Delete ejs directory

--- ejs/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const server= require('./src/app')
-
-const app = express()
-
-const PORT = 8080
-
-server.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
--- ejs/src/Router/routes.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require("express")
-
-const myRoutes = Router()
-
-myRoutes.get('/',(req,res)=>{
-    res.render('index')
-})
-
-module.exports = myRoutes
