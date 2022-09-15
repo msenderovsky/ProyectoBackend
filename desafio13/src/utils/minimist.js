f3ee918dbce4db210405c1a9b2fb51fdd3b5dabd
msenderovsky <msenderovsky@gmail.com>
2022-09-15 15:38:03 -0300

    Delete desafio13 directory

--- desafio13/src/utils/minimist.js
+++
@@ -1,6 +0,0 @@
-import parseArgs from 'minimist'
-
-const options = { default: {puerto: 8080}}
-export const PORT = parseArgs(process.argv, options).puerto
-
-console.log(`Mi puerto de conexion es ${PORT}`)
