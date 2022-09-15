b7061293bf3973e323a40d26a653f0d433ab9dd2
msenderovsky <msenderovsky@gmail.com>
2022-09-15 15:40:46 -0300

    Delete desafio19 directory

--- desafio19/src/routers/newRoute.js
+++
@@ -1,5 +0,0 @@
-const { Router } = require('express')
-const { router } = require('../app')
-const productsController = require('../controllers/products')
-
-router.get("/productos", productsController.getProducts)
